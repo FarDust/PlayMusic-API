d9177786893a69dd39112fa325e8da0287882e97
Gabriel Faundez <gnfaundez@uc.cl>
2017-10-20 16:54:20 -0300

    first approach

--- playmusic/mod_api/views.py
+++ playmusic/mod_api/views.py
@@ -1,10 +1,22 @@
 from playmusic import db
-from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import Blueprint, request, render_template, flash, redirect, url_for, jsonify
 import flask_bootstrap
+import json
 
-mod_api = Blueprint('auth',__name__,url_prefix='/api')
+mod_api = Blueprint('auth', __name__, url_prefix='/api')
 
-@mod_api.route('/<string:post_id>', methods=['GET','POST'])
+
+@mod_api.route('/<string:post_id>', methods=['GET', 'POST'])
 def index(post_id):
     #db.db.users.find({'_id': post_id})
     return render_template("api/index.html")
+
+
+@mod_api.route('/menssages/<id>', methods=["GET"])
+def mensajes(r_id):
+    return jsonify(db["menssage"].find_one({"id": r_id}, {"_id": 0})), 200
+
+
+@mod_api.route('/artits/<id>', methods=["GET"])
+def artista(r_id):
+    return jsonify(db["artist"].fin_one({"id": r_id}, {"_id": 0}))
--- playmusic/views.py
+++ playmusic/views.py
@@ -1,9 +1,13 @@
 from playmusic import app
-from flask import render_template
+from flask import render_template, request
 from pymongo import *
 import flask_bootstrap
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    bar = request.args.to_dict()
+    if "a" in bar and "b" in bar:
+        return str(bar["a"]) + " " + str(bar["b"]), 200
+    else:
+        return render_template("404.html"), 404
