429c61a09e730a7e8ea741efc2a86b050f3de5f9
eavillagran <eavillagran@uc.cl>
2017-10-27 23:26:49 -0300

    agregar tercera consulta

--- playmusic/__init__.py
+++ playmusic/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
-from flask import Response, send_from_directory, 
-                                render_template, request
+from flask import Response, send_from_directory, render_template, request
 from flask_pymongo import PyMongo
 import json
 # Importar modulo de la base de datos
@@ -33,10 +32,53 @@ def mensajes_id(id):
 
     except Exception as e:
         status = 500
-        ret = {'Error': 'Ocurrio un error.'}
+        ret = {'error': 'Ocurrio un error.'}
 
     finally:
         respuesta = Response(json.dumps(ret), status = status, mimetype='application/json') 
+        return respuesta
+
+
+@app.route('/artista/<id>')
+def artista_id(id):
+    """
+    Segunda ruta del enunciado. Recibe el id del artista y retorna
+    toda la informacion del usuario y una lista con todos los 
+    mensajes enviados por el usuario
+    """
+    try:
+        database = db()
+        ret = database.buscar_artista_id(int(id))
+        status = 200
+
+    except Exception as e:
+        status = 500
+        ret = {'error': 'Ocurrio un error.'}
+
+    finally:
+        respuesta = Response(json.dumps(ret), status=status, mimetype='application/json')
+        return respuesta
+
+
+@app.route('/artista/<id_1>/<id_2>')
+def artistas_id(id_1, id_2):
+    """
+    Tercera ruta del enunciado. Recibe los dos ids de los artistas y 
+    retorna una lista con todas los mensajes que han intercambiado 
+    ambos artistas,
+    """
+    try:
+        database = db()
+        ret = database.mensajes_compartidos(int(id_1), int(id_2))
+        status = 200
+
+    except Exception as e:
+        print('[ERROR] Ocurrió un error en mensajes de artistas', e)
+        status = 500
+        ret = {'error': 'Ocurrio un error.'}
+
+    finally:
+        respuesta = Response(json.dumps(ret), status=status, mimetype='application/json')
         return respuesta
 
 
