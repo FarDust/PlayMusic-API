40129d42192b99a8af632644ab1cd3ef5e9ca46a
eavillagran <eavillagran@uc.cl>
2017-10-27 22:37:21 -0300

    crear dos rutas

--- playmusic/__init__.py
+++ playmusic/__init__.py
@@ -1,16 +1,48 @@
-from flask import Flask,render_template
+from flask import Flask
+from flask import Response, send_from_directory, 
+                                render_template, request
 from flask_pymongo import PyMongo
+import json
+# Importar modulo de la base de datos
+from playmusic.db import Database as db
 
 app = Flask(__name__)
-
 app.config.from_object('config')
 
-db = PyMongo(app)
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'),404
+@app.route('/')
+def inicio():
+    """
+    Ruta de inicio de la API. Retorna un simple string
+    de bievenida.
+    """
+    return 'Bienvenido a PlayMusic'
 
+
+@app.route('/mensajes/<id>')
+def mensajes_id(id): 
+    """
+    Primera rota del enunciado. Recibe el id de un mensaje
+    y retorna toda la informacion de este mensaje.
+    """
+    try: 
+        database = db()
+        id = int(id)
+        ret = database.buscar_mensaje_id(id)
+        status = 200
+
+    except Exception as e:
+        status = 500
+        ret = {'Error': 'Ocurrio un error.'}
+
+    finally:
+        respuesta = Response(json.dumps(ret), status = status, mimetype='application/json') 
+        return respuesta
+
+
+
+# Esto no entendí para que sirve, mejor quizás no complicarse
+# con tanto y hacer una api sencilla no más
 from playmusic.mod_api.views import mod_api as api_module
 
 app.register_blueprint(api_module)
