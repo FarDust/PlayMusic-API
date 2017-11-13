81dbcbb583b401b7dac8f0f007428294cfaee2f0
Gabriel Faundez <gnfaundez@uc.cl>
2017-11-13 12:00:19 -0300

    eliminando ortografia

--- playmusic/db.py
+++ playmusic/db.py
@@ -23,11 +23,11 @@ class Database:
             self.db = self.cliente[DATABASE]
         # Malas practicas para manejar errores
         except Exception as e:
-            print('[ERROR] Ocurrió un error al intentar conectarse: ', e)
+            print('[ERROR] Ocurrio un error al intentar conectarse: ', e)
 
     def buscar_mensaje_id(self, id):
         """
-        Metodo para buscar toda la informacion de un mensaje según su
+        Metodo para buscar toda la informacion de un mensaje segun su
         id. Recibe el id del mensaje, y retorna lo entregado por la base de
         datos.
         """
@@ -37,15 +37,15 @@ class Database:
             respuesta = {'entities': resultado}
 
         except Exception as e:
-            print('[ERROR] Ocurrió un error al buscar informacion del mensaje', e)
-            respuesta = {'error': 'Ocurrió un error.'}
+            print('[ERROR] Ocurrio un error al buscar informacion del mensaje', e)
+            respuesta = {'error': 'Ocurrio un error.'}
 
         finally:
             return respuesta
 
     def buscar_artista_id(self, id):
         """
-        Metodo para buscar toda la informacion del artista entregado. Además
+        Metodo para buscar toda la informacion del artista entregado. Ademas
         entrega una lista con todos los mensajes emitidos por el artista
         """
         try:
@@ -54,13 +54,13 @@ class Database:
             informacion_usuario = self.db.users.find({'_id': id})
             informacion_usuario = json.loads(
                 json_util.dumps(informacion_usuario))
-            # Respuesta que se entregará
+            # Respuesta que se entregara
             respuesta = {'informacion': informacion_usuario,
                          'mensajes': mensajes}
 
         except Exception as e:
-            print('[ERROR] Ocurrió un un error al intentar buscar info de artista', e)
-            respuesta = {'error': 'Ocurrió un error.'}
+            print('[ERROR] Ocurrio un un error al intentar buscar info de artista', e)
+            respuesta = {'error': 'Ocurrio un error.'}
 
         finally:
             return respuesta
@@ -82,8 +82,8 @@ class Database:
             respuesta = {'mensajes': mensajes}
 
         except Exception as e:
-            print('[ERROR] Ocurrió un error al intentar buscar mensajes', e)
-            respuesta = {'error': 'Ocurrió un error en la base de datos.'}
+            print('[ERROR] Ocurrio un error al intentar buscar mensajes', e)
+            respuesta = {'error': 'Ocurrio un error en la base de datos.'}
 
         finally:
             return respuesta
@@ -91,15 +91,15 @@ class Database:
     def mensajes_filtrados(self, obligatorios, quizas, no_pueden):
         """
         Metodo para filtrar mensajes. Recibe una lista con frases obligatorias,
-        una lista con palabras que quizás podrían estar y una lista con palabras
+        una lista con palabras que quizas podrian estar y una lista con palabras
         que no pueden estar en los mensajes.
-        Después se escribe una consulta como un string y después se ejecuta
+        Despues se escribe una consulta como un string y despues se ejecuta
         con eval() y se retorna la lista de mensajes.
         """
         try:
             # Base de la query se se ejecutara
             query_aux = "self.db.messages.find({\"$text\": {\"$search\": \""
-            # Concatenar palabras que quizás pueden ir
+            # Concatenar palabras que quizas pueden ir
             for x in quizas:
                 query_aux += "{} ".format(x)
             # Concatenar frases obligatorias que deben estar en el mensaje
@@ -118,8 +118,8 @@ class Database:
             respuesta = {'mensajes': mensajes}
 
         except Exception as e:
-            print('[ERROR] Ocurrió un error al intentar buscar mensajes', e)
-            respuesta = {'error': 'Ocurrió un error en la base de datos.'}
+            print('[ERROR] Ocurrio un error al intentar buscar mensajes', e)
+            respuesta = {'error': 'Ocurrio un error en la base de datos.'}
 
         finally:
             return respuesta
